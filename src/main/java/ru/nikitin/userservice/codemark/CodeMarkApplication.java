ac1ee42bf14da38d59e15adfd68e03628c4106f9
Serg-Nikitin <nikitin879@yandex.ru>
2022-09-26 11:26:28 +0500

    empty project commit

---
+++ src/main/java/ru/nikitin/userservice/codemark/CodeMarkApplication.java
@@ -0,0 +1,13 @@
+package ru.nikitin.userservice.codemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nikitin/userservice/codemark/CodeMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikitin.userservice.codemark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
