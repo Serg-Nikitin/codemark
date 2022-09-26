bbe02ea3a6780b95aa9e550c683a556333586899
Serg-Nikitin <nikitin879@yandex.ru>
2022-09-26 11:57:22 +0500

    added postgresql DB

---
+++ src/main/java/ru/nikitin/userservice/codemark/config/Config.java
@@ -0,0 +1,10 @@
+package ru.nikitin.userservice.codemark.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Config {
+}
