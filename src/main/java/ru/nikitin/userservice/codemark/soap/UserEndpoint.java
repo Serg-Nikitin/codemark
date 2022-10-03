ab8f3db15faa8bc436a3e882e76b9b6c2ca58f6e
Serg-Nikitin <nikitin879@yandex.ru>
2022-10-03 10:09:02 +0500

    simple implementation

--- src/main/java/ru/nikitin/userservice/codemark/soap/UserEndpoint.java
+++ src/main/java/ru/nikitin/userservice/codemark/soap/UserEndpoint.java
@@ -2,22 +2,14 @@ package ru.nikitin.userservice.codemark.soap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.validation.BindingResult;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import ru.nikitin.userservice.codemark.*;
 import ru.nikitin.userservice.codemark.service.UserService;
-import ru.nikitin.userservice.codemark.soap.validation.UserSoap1;
-import ru.nikitin.userservice.codemark.soap.xml.*;
-import ru.nikitin.userservice.codemark.to.UserTo;
-
-import javax.validation.Valid;
-import javax.xml.bind.Marshaller;
 
 import static ru.nikitin.userservice.codemark.soap.UserServiceConfig.NAME_SPACE_URI;
-import static ru.nikitin.userservice.codemark.to.UserTo.getToFromRequest;
 
 @Endpoint
 public class UserEndpoint {
@@ -36,20 +28,15 @@ public class UserEndpoint {
     @ResponsePayload
     public GetUsersResponse getAll() {
         GetUsersResponse resp = new GetUsersResponse();
-        resp.getUsers().addAll(service.getAll());
+
         return resp;
     }
 
     @PayloadRoot(namespace = NAME_SPACE_URI,
             localPart = "getUserByLoginRequest")
     @ResponsePayload
-    public Marshaller getByLogin(@RequestPayload GetUserByLoginRequest request) {
-  /*      if(result.hasErrors()){
-            log.info(result.toString());
-        }*/
-
+    public GetUserByLoginResponse getByLogin(@RequestPayload GetUserByLoginRequest request) {
         GetUserByLoginResponse resp = new GetUserByLoginResponse();
-        resp.setUser(service.getUserWithRole(request.getLogin()));
         return resp;
     }
 
@@ -59,9 +46,8 @@ public class UserEndpoint {
     @ResponsePayload
     public CreateUserResponse create(@RequestPayload CreateUserRequest request) {
         CreateUserResponse resp = new CreateUserResponse();
-        UserTo userTo = getToFromRequest(request.getUser());
 
-        resp.setUser(service.create(userTo));
+
         return resp;
     }
 
@@ -70,9 +56,8 @@ public class UserEndpoint {
     @ResponsePayload
     public UpdateUserResponse update(@RequestPayload UpdateUserRequest request) {
         UpdateUserResponse resp = new UpdateUserResponse();
-        UserTo userTo = getToFromRequest(request.getUser());
 
-        resp.setUser(service.update(request.getLogin(), userTo));
+
         return resp;
     }
 
@@ -82,7 +67,7 @@ public class UserEndpoint {
     public DeleteUserByLoginResponse delete(@RequestPayload DeleteUserByLoginRequest request) {
         String login = request.getLogin();
         DeleteUserByLoginResponse resp = new DeleteUserByLoginResponse();
-        resp.setDelete(service.deleteUser(login));
+
         return resp;
     }
 
